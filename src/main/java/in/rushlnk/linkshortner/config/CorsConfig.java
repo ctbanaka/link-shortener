ba3497197d187c1be910e3129ecbb7a2e1f19922
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2024-10-19 23:38:58 +0530

    config changes

---
+++ src/main/java/in/rushlnk/linkshortner/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package in.rushlnk.linkshortner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Apply to all endpoints
+                        .allowedOrigins("http://localhost:8000", "https://rushlnk.in")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600); // 1 hour max age
+            }
+        };
+    }
+
+
+}
